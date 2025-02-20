a3cd5ddaeca12bb5608fb750c57c15cd2f9cf3fb
kwame <kwamebb36@gmail.com>
2025-02-20 11:17:35 -0500

    starting JS tutorial

---
+++ tutorial4/moz-todo-react/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const subject = "React";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Hello, {subject}!</h1>
+        <button type="button">Click me!</button>
+      </header>
+    </>
+  );
+}
+
+export default App;
